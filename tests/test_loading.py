e40c8099b785375ba98d38cc88a0da452a9dba9b
gtonks <a02288558@usu.edu>
2023-07-11 12:59:10 -0600

    One more `get_agents()` test looking sepcifically at `ev_params`

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -57,7 +57,7 @@ def test_get_agents2():
     return True
 
 
-def test_get_agents3():
+def test_get_agents_color():
     agent_classes = get_agents(os.path.join('tests', 'input', 'agents3.csv'))
     try:
         assert agent_classes[0]['color'] == '0,0,0'
@@ -70,11 +70,25 @@ def test_get_agents3():
     return True
 
 
+def test_get_agents_ev_params():
+    agent_classes = get_agents(os.path.join('tests', 'input', 'agents4.csv'))
+    try:
+        assert agent_classes[0]['ev_params']['A_f'] == 2.22
+        assert agent_classes[1]['ev_params']['A_f'] == 2.3316
+    except AssertionError:
+        traceback.print_exc()
+        print(f"{agent_classes=}")
+        print()
+        return False
+    return True
+
+
 if __name__ == "__main__":
     tests = (
         test_get_agents1, 
         test_get_agents2, 
-        test_get_agents3,
+        test_get_agents_color,
+        test_get_agents_ev_params,
     )
     success = 0
     total = 0
