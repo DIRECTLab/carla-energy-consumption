a2062c856b281b368045f13e62fdba7ee49592dd
gtonks <a02288558@usu.edu>
2023-07-18 16:47:32 -0600

    Make coordinates.py more robust

--- coordinates.py
+++ coordinates.py
@@ -3,16 +3,29 @@ import time
 import carla
 
 
-def show_coordinates(debug:carla.DebugHelper, time=0.1):
+def get_elevation(location:carla.Location, world:carla.World):
     """
-    Highlights x- and y-axes at the origin.
+    Returns the height of the surface at `location`.
     """
-    height = 20
-    x_end = carla.Location(20,0,height)
-    debug.draw_arrow(carla.Location(0,0,height), x_end, thickness=1, arrow_size=1, color=carla.Color(255,200,0), life_time=time)
+    max_height = 99_999    # Max height (meters)
+    location.z = max_height
+    point = world.ground_projection(location, max_height * 2)
+    if point is None: return
+    return point.location.z
+
+
+def show_coordinates(world:carla.World, time=0.1):
+    """
+    Highlights x- and y-axes at (0,0).
+    """
+    debug = world.debug
+    height = get_elevation(carla.Location(), world) + 20
+    length = 20
+    x_end = carla.Location(length,0,height)
+    debug.draw_arrow(carla.Location(0,0,height), x_end, thickness=1, arrow_size=2.5, color=carla.Color(255,200,0), life_time=time)
     debug.draw_string(x_end + carla.Vector3D(0,2,0), 'x', life_time=time)
-    y_end = carla.Location(0,20,height)
-    debug.draw_arrow(carla.Location(0,0,height), y_end, thickness=1, arrow_size=1, color=carla.Color(255,200,0), life_time=time)
+    y_end = carla.Location(0,length,height)
+    debug.draw_arrow(carla.Location(0,0,height), y_end, thickness=1, arrow_size=2.5, color=carla.Color(255,200,0), life_time=time)
     debug.draw_string(y_end + carla.Vector3D(0,2,0), 'y', life_time=time)
 
 
@@ -37,7 +50,7 @@ if __name__ == '__main__':
     client.set_timeout(20.0)
     world = client.get_world()
 
-    tick_id = world.on_tick(lambda snapshot: show_coordinates(world.debug))
+    tick_id = world.on_tick(lambda snapshot: show_coordinates(world))
     try:
         print('Waiting for Ctrl-C')
         time.sleep(3600)
