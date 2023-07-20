f64c381352692e560eab2e74731ffc37072ff7fc
gtonks <a02288558@usu.edu>
2023-07-20 17:22:57 -0600

    `power_to_vehicle` follows parabola; waiting on `get_lateral_misalignment`

--- trackers/charger.py
+++ trackers/charger.py
@@ -21,13 +21,20 @@ class Charger:
         extent = Vector3D(length / 2, width / 2, 2)     # z-axis is hard-coded at +/- 2 m.
         self.bbox = BoundingBox(Location(), extent)
         self.transform = transform
-        self.effective_power = efficiency * power
+        self.max_power = efficiency * power
+        self.a = - self.max_power / (width/2)**2
+
+    def get_lateral_misalignment(self, point:Location) -> float:
+        """
+        Return the lateral misalignment of `point` from this charger.
+        """
 
     def power_to_vehicle(self, point:Location) -> float:
         """
         Determine the power delivered to the vehicle in Watts, assuming the center of the vehicle is at `point`.
         """
         if self.bbox.contains(point, self.transform):
-            return self.effective_power
+            lat_misalignment = self.get_lateral_misalignment()
+            return self.a * lat_misalignment**2 + self.max_power
         else:
             return 0.0
