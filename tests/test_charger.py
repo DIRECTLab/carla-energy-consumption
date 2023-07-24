3a726ca84a1851cc03d17933f031ea784c9cbbdb
gtonks <a02288558@usu.edu>
2023-07-24 14:45:12 -0600

    Add more tests, including some for power

--- tests/test_charger.py
+++ tests/test_charger.py
@@ -77,6 +77,25 @@ def test_transform_in4():
 
 
 def test_transform_in5():
+    # Length=2, width=2, 30% grade
+    # I don't know for sure, but I think this is an accuracy problem
+    front_left = Location(-0.977241014286, -0.977241014286, 0.3)
+    front_right = Location(0.977241014286, -0.977241014286, 0.3)
+    back_right = Location(0.977241014286, 0.977241014286, -0.3)
+    charger = Charger(front_left, front_right, back_right, 0, 0)
+    point = Location(-0.977241014286, 0.977241014286, -0.3)   # the missing corner
+    transformed = charger.transform_in(point)
+    try:
+        assert transformed.distance(Location(-1.0, 1.0, 0.0)) < 0.01
+    except AssertionError:
+        traceback.print_exc()
+        print(f"transformed=({transformed.x}, {transformed.y}, {transformed.z})")
+        print()
+        return False
+    return True
+
+
+def test_transform_in6():
     # Length=2, width=2, 30% grade rotated 45 degrees
     # I don't know for sure, but I think this is an accuracy problem
     front_left = Location(0, -1.38202749611, 0.3)
@@ -84,13 +103,80 @@ def test_transform_in5():
     back_right = Location(0, 1.38202749611, -0.3)
     charger = Charger(front_left, front_right, back_right, 0, 0)
     point = Location(-1.38202749611, 0, -0.3)   # the missing corner
-    print(point.x, point.y, point.z)
     transformed = charger.transform_in(point)
     try:
         assert transformed.distance(Location(-1.0, 1.0, 0.0)) < 0.01
     except AssertionError:
         traceback.print_exc()
         print(f"transformed=({transformed.x}, {transformed.y}, {transformed.z})")
+        print()
+        return False
+    return True
+
+
+def test_power_to_vehicle1():
+    front_left = Location(-1,0,0)
+    front_right = Location(0,0,0)
+    back_right = Location(0,3,0)
+    charger = Charger(front_left, front_right, back_right, 0, 0.85)
+    point = Location(-0.5,1.5,0)
+    power = charger.power_to_vehicle(point)
+    try:
+        assert power == 0.0
+    except AssertionError:
+        traceback.print_exc()
+        print(f"{power=}")
+        print()
+        return False
+    return True
+
+
+def test_power_to_vehicle2():
+    front_left = Location(-1,0,0)
+    front_right = Location(0,0,0)
+    back_right = Location(0,3,0)
+    charger = Charger(front_left, front_right, back_right, 30_000, 0)
+    point = Location(-0.5,1.5,0)
+    power = charger.power_to_vehicle(point)
+    try:
+        assert power == 0.0
+    except AssertionError:
+        traceback.print_exc()
+        print(f"{power=}")
+        print()
+        return False
+    return True
+
+
+def test_power_to_vehicle3():
+    front_left = Location(-1,0,0)
+    front_right = Location(0,0,0)
+    back_right = Location(0,3,0)
+    charger = Charger(front_left, front_right, back_right, 30_000, 0.85)
+    point = Location(-1,1.5,0)
+    power = charger.power_to_vehicle(point)
+    try:
+        assert power == 0
+    except AssertionError:
+        traceback.print_exc()
+        print(f"{power=}")
+        print()
+        return False
+    return True
+
+
+def test_power_to_vehicle4():
+    front_left = Location(-1,0,0)
+    front_right = Location(0,0,0)
+    back_right = Location(0,3,0)
+    charger = Charger(front_left, front_right, back_right, 30_000, 0.85)
+    point = Location(-0.5,1.5,0)
+    power = charger.power_to_vehicle(point)
+    try:
+        assert power == 25_500.0
+    except AssertionError:
+        traceback.print_exc()
+        print(f"{power=}")
         print()
         return False
     return True
@@ -103,6 +189,11 @@ if __name__ == "__main__":
         test_transform_in3,
         test_transform_in4,
         test_transform_in5,
+        test_transform_in6,
+        test_power_to_vehicle1,
+        test_power_to_vehicle2,
+        test_power_to_vehicle3,
+        test_power_to_vehicle4,
     )
     success = 0
     total = 0
