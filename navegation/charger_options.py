68b8ab8ff8315ca19fa94f569db2eb3eb7a1c093
gtonks <a02288558@usu.edu>
2023-07-31 15:30:52 -0600

    Refactor `charger_options`, adding `create_charger()` and `display_options()`

--- navegation/charger_options.py
+++ navegation/charger_options.py
@@ -9,6 +9,7 @@ import time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from trackers.charger import Charger
+# from loading import parse_location
 
 
 def get_charger_options(world:carla.World, length:float, width:float) -> list:
@@ -24,18 +25,32 @@ def get_charger_options(world:carla.World, length:float, width:float) -> list:
         center = world.project_point(spawn_point.location, down_unit, 5.0).location
         if center is None:
             center = spawn_point.location
-
-        forward_unit = rotation.get_forward_vector()
-        forward = forward_unit * length / 2
-        right_unit = rotation.get_right_vector()
-        right = right_unit * width / 2
-
-        front_left = center + forward - right
-        front_right = center + forward + right
-        back_right = center + right - forward
-        charger = Charger(front_left, front_right, back_right, power=0.0, efficiency=0.0)
+        transform = carla.Transform(center, rotation)
+        charger = create_charger(length, width, transform)
         options.append(charger)
     return options
+
+
+def create_charger(length:float, width:float, center_transform:carla.Transform) -> Charger:
+    forward_unit = center_transform.rotation.get_forward_vector()
+    forward = forward_unit * length / 2
+    right_unit = center_transform.rotation.get_right_vector()
+    right = right_unit * width / 2
+
+    front_left = center_transform.location + forward - right
+    front_right = center_transform.location + forward + right
+    back_right = center_transform.location + right - forward
+    return Charger(front_left, front_right, back_right, power=0.0, efficiency=0.0)
+
+
+def display_options(options:list, interval:float):
+    print('front_left,front_right,back_right')
+    for charger in options:
+        charger.draw(world.debug, interval)
+        print(f'"({charger.front_left.x},{charger.front_left.y},{charger.front_left.z})",', end='')
+        print(f'"({charger.front_right.x},{charger.front_right.y},{charger.front_right.z})",', end='')
+        print(f'"({charger.back_right.x},{charger.back_right.y},{charger.back_right.z})"')
+        time.sleep(interval)
 
 
 if __name__ == '__main__':
@@ -52,6 +67,12 @@ if __name__ == '__main__':
         type=float,
         help='width of the charger'
     )
+    # argparser.add_argument(
+    #     '-p', '--point',
+    #     metavar='(X,Y,Z)',
+    #     type=parse_location,
+    #     help='approximate charger center location'
+    # )
     argparser.add_argument(
         '-i', '--interval',
         metavar='I',
@@ -78,12 +99,8 @@ if __name__ == '__main__':
     client.set_timeout(20.0)
     world = client.get_world()
 
+    # if args.point is None:
     options = get_charger_options(world, args.length, args.width)
     random.shuffle(options)
-    print('front_left,front_right,back_right')
-    for charger in options:
-        charger.draw(world.debug, args.interval)
-        print(f'"{charger.front_left.x},{charger.front_left.y},{charger.front_left.z}",', end='')
-        print(f'"{charger.front_right.x},{charger.front_right.y},{charger.front_right.z}",', end='')
-        print(f'"{charger.back_right.x},{charger.back_right.y},{charger.back_right.z}"')
-        time.sleep(args.interval)
+
+    display_options(options, args.interval)
