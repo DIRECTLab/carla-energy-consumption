899cbf93297412bca15640282a0b855da2501590
gtonks <a02288558@usu.edu>
2023-08-11 14:31:36 -0600

    fields can be customized and randomized

--- input/prepare_agents.py
+++ input/prepare_agents.py
@@ -1,6 +1,31 @@
 import argparse
 import sys
+import random
 import csv
+
+
+def agent_fields(args) -> dict:
+    fields = dict()
+    for idx in range(0, len(args), 2):
+        try:
+            fields[args[idx]] = args[idx+1]
+        except IndexError:
+            print('Error: Each constant field must provide a name and a value.')
+            sys.exit(1)
+    return fields
+
+
+def prepare_agents(number, vehicle, agent_type, randomized_fields=list(), **kwargs) -> dict:
+    agents = list()
+    for _ in range(number):
+        agent = kwargs.copy()
+        agent['vehicle'] = vehicle
+        agent['agent_type'] = agent_type
+        for field in randomized_fields:
+            if field == 'init_soc':
+                agent['init_soc'] = random.random()
+        agents.append(agent)
+    return agents
 
 
 if __name__ == "__main__":
@@ -13,32 +38,38 @@ if __name__ == "__main__":
     )
     argparser.add_argument(
         'vehicle',
+        metavar='VEHICLE',
         help='vehicle blueprint'
     )
     argparser.add_argument(
         'agent_type',
+        metavar='AGENT_TYPE',
         help='agent type'
     )
     argparser.add_argument(
-        '--init_soc',
-        help='add random init_soc parameter'
+        '-c', '--constants',
+        metavar='FIELD VALUE',
+        nargs='*',
+        default=list(),
+        help='any other fields with constant values, e.g. init_soc 0.8'
     )
     argparser.add_argument(
-        'out',
-        nargs='?',
+        '-r', '--randomize',
+        metavar='FIELD',
+        nargs='*',
+        default=list(),
+        help='fields to randomize values for'
+    )
+    argparser.add_argument(
+        '-o', '--out',
         type=argparse.FileType('w'),
-        default=sys.stdout
+        default=sys.stdout,
+        help='file to write agents to (default: stdout)'
     )
     args = argparser.parse_args()
+    args.constants = agent_fields(args.constants)
 
-    agents = list()
-    for _ in range(args.number):
-        agent = {
-            'vehicle': args.vehicle,
-            'agent_type': args.agent_type,
-        }
-        agents.append(agent)
-
-
-    writer = csv.DictWriter(args.out, ['vehicle', 'agent_type'])
+    agents = prepare_agents(args.number, args.vehicle, args.agent_type, args.randomize, **args.constants)
+    writer = csv.DictWriter(args.out, agents[0].keys())
+    writer.writeheader()
     writer.writerows(agents)
