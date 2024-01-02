db4e57482eab86193f2e4908d5070b08341ffd0a
gtonks <a02288558@usu.edu>
2024-01-02 13:33:26 -0700

    Ensure float division

--- navigation/charger_stuff.py
+++ navigation/charger_stuff.py
@@ -13,9 +13,9 @@ from interface.trackers.charger import Charger
 
 def create_charger(length:float, width:float, center_transform:carla.Transform, power=0.0, efficiency=0.0) -> Charger:
     forward_unit = center_transform.rotation.get_forward_vector()
-    forward = forward_unit * length / 2
+    forward = forward_unit * length / 2.0
     right_unit = center_transform.rotation.get_right_vector()
-    right = right_unit * width / 2
+    right = right_unit * width / 2.0
 
     front_left = center_transform.location + forward - right
     front_right = center_transform.location + forward + right
