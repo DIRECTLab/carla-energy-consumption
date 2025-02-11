a8a862dd198bf5aac771e694bf77dcaf7341cbf6
Isaac Peterson <isaacwilliam4@gmail.com>
2025-02-11 11:51:39 -0700

    fixed run carla script

--- run_carla.sh
+++ run_carla.sh
@@ -13,5 +13,5 @@ else
 fi
 
 export RES=$(xrandr | grep '*' | awk '{print $1}')
-python3 manual_control_steeringwheel.py ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES 
-python3 navigation/draw_chargers.py ./input/examples/Town10_intersection_chargers.csv 
+python3 manual_control_steeringwheel.py ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
+python3 navigation/draw_chargers.py ./input/examples/Town10_intersection_chargers.csv
