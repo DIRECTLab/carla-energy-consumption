64c9dc4fd4373ac5282f0df528dd9fd3fe031a24
nosyt-dev <tyson.buxton@usu.edu>
2025-05-16 07:14:05 -0600

    Added options and handling to the script, to make it easier to pick different vehicles and screen resolutions.

--- run_carla.sh
+++ run_carla.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
-#I made some edits to this script to start getting our extension working with carla unreal engine 5
 eval "$(conda shell.bash hook)"
 
-#TODO add a way to the command line to choose which vehicle csv to run.
+show_help() {
+  echo "Usage: $0 [-v VEHICLE] [-h]"
+  echo ""
+  echo "Leave with no options to use defaults"
+  echo ""
+  echo "Options:"
+  echo "  -v VEHICLE Specify name of the vehicle csv to load. Ie. for kenworth.csv, enter kenworth"
+  echo "  -r RES Specify the desired resolution for the client screen. Ie. 1920x1080"
+  echo "  -h         Show this help message and exit."
+}
+
+#Default values
+export RES=$(xrandr | grep '*' | awk '{print $1}')
+VEHICLE=lincoln
+
+#Handles the options
+while getopts "v:r:h" opt; do
+  case $opt in
+    v) VEHICLE=$OPTARG ;;
+    r) RES=$OPTARG ;;
+    h) show_help; exit 0 ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
 
 if conda env list | grep -qw "client-carlaUE5"; then
   echo "Carla client conda env exists, skipping installation..."
@@ -17,10 +39,5 @@ else
                                                                                                   #the local wheel file in the Carla server Build
 fi
 
-export RES=$(xrandr | grep '*' | awk '{print $1}')
-# export RES="1920x1080"
-# export RES="1366x768"
-# export RES="1440x900"
-
-python3 manual_control_steeringwheel.py ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
+python3 manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
 python3 navigation/draw_chargers.py ./input/examples/Town10_intersection_chargers.csv
