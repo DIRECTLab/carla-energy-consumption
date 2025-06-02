d94ac6efec4c067c10d163609c6ab9835e613eba
zac <zac.maughan@usu.edu>
2025-06-02 09:11:03 -0600

    Adding IP address

--- run_carla.sh
+++ run_carla.sh
@@ -13,5 +13,5 @@ else
 fi
 
 export RES=$(xrandr | grep '*' | awk 'NR==1 {print $1}')
-python3 manual_control_steeringwheel.py ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
+python3 manual_control_steeringwheel.py --host 144.39.45.43 ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
 python3 navigation/draw_chargers.py ./input/examples/Town10_intersection_chargers.csv
