3f8236c5d27245736798abc33b1d6f0556ae00dc
nosyt-dev <tyson.buxton@usu.edu>
2025-08-28 16:57:16 -0600

    Updated run_carla.sh to include options/flgas

--- run_carla.sh
+++ run_carla.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 
+
+show_help() {
+  echo "Usage: $0 [-v VEHICLE] [-r RES] [-h]"
+  echo ""
+  echo "Options:"
+  echo "  -v  VEHICLE       Specify name of the vehicle csv to load. Ie. for kenworth.csv, enter kenworth."
+  echo "  -r  WIDTHxHEIGHT  Specify the desired resolution for the client screen. Ie. 1920x1080"
+  echo "  -d                Debug Mode. Show all output from commands."
+  echo "  -h                Show this help message and exit."
+}
+
+#Default values
+#NR is which monitor's resolution to start pygame with, 1 is the primary monitor.
+export RES=$(xrandr | grep '*' | awk 'NR==1 {print $1}')
+VEHICLE=tesla
+DEBUG=false
+
+#Handles the options
+while getopts "v:s:r:dh" opt; do
+  case $opt in
+    v) VEHICLE=$OPTARG ;;
+    r) RES=$OPTARG ;;
+    d) DEBUG=true ;;
+    h) show_help; exit 0 ;;
+    *) echo "Invalid option"; exit 1 ;;
+  esac
+done
+
 if conda env list | grep -qw "carlaenv"; then
   echo "Carla conda env exists, skipping installation..."
   conda activate carlaenv
@@ -12,9 +40,12 @@ else
   pip install carla==0.9.14
 fi
 
-export RES=$(xrandr | grep '*' | awk 'NR==1 {print $1}')
-# python3 manual_control_steeringwheel.py ./input/examples/kenworth.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
-python3 manual_control_steeringwheel.py ./input/examples/tesla.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
+if $DEBUG; then
+  python3 manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES;
+else
+  python3 manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/Town10_intersection_chargers.csv --res $RES > /dev/null 2>&1 &
+fi
+
 python3 navigation/draw_chargers.py ./input/examples/Town10_intersection_chargers.csv
 
 # Wi-Fi IP Address: 144.39.45.43
