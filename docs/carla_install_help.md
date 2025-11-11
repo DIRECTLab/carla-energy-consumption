4c038d176f31cee5c2cc8a1aca3ae82323749d5f
nosyt-dev <tyson.buxton@usu.edu>
2025-11-11 10:51:32 -0700

    Slight fix on some documentation. Also the run_carla script now has an option to run the automated simulation demo for you at least on map 10. Have not tested on other maps.

--- docs/carla_install_help.md
+++ docs/carla_install_help.md
@@ -7,10 +7,10 @@ from source in Linux. After making sure you have all of the correct requirements
 make sure that when you are cloning CARLA that you are 
 getting version 0.9.14. Use the following command after downloading aria2:
 `git clone -b 0.9.14 https://github.com/carla-simulator/carla.git`.
-1. After cloning CARLA, you need to have the assets specific to CARLA 0.9.14.
+0. After cloning CARLA, you need to have the assets specific to CARLA 0.9.14.
 This requires that you go into the Update.sh and replacing the URL on the 50th line
 with this URL: `https://carla-assets.s3.us-east-005.backblazeb2.com/${CONTENT_ID}.tar.gz`
-2. You are then going to run `make PythonAPI`. The first time probably won't work
+0. You are then going to run `make PythonAPI`. The first time probably won't work
 due to the boost file. You are going to have to download it yourself and placing it into
 the Build file in VScode. You can find the correct boost file by going to the following URL:
 `https://www.boost.org/users/history/version_1_80_0.html`. **Make sure that you delete the 
@@ -18,5 +18,5 @@ the Build file in VScode. You can find the correct boost file by going to the fo
 boost file is bad if you open it and you are able to read text in your text editor.
 It won't recognize the boost you downloaded unless you do it otherwise. 
 You will likely have to do this process every time that you run `make ...`
-3. Run `make launch`
-4. The CARLA server with the Unreal Engine Editor should pop up and you're good to go!
+0. Run `make launch`
+0. The CARLA server with the Unreal Engine Editor should pop up and you're good to go!
--- run_carla.sh
+++ run_carla.sh
@@ -6,6 +6,7 @@ show_help() {
   echo "Usage: $0 [-v VEHICLE] [-r RES] [-h]"
   echo ""
   echo "Options:"
+  echo "  -a                Runs the automated simulation demo. Cannot be combined with other options."
   echo "  -v  VEHICLE       Specify name of the vehicle csv to load. Ie. for kenworth.csv, enter kenworth."
   echo "  -c  CHARGERS      Specify the name of the csv file with the corresponding map's chargers."
   echo "  -r  WIDTHxHEIGHT  Specify the desired resolution for the client screen. Ie. 1920x1080"
@@ -14,18 +15,19 @@ show_help() {
 }
 
 #Default values
-#NR is which monitor's resolution to start pygame with, 1 is the primary monitor.
 export RES=$(xrandr | grep '*' | awk 'NR==1 {print $1}')
 VEHICLE=tesla
 CHARGERS=Town10_intersection_chargers
 DEBUG=false
+AUTOMATED_DEMO=false
 
 #Handles the options
-while getopts "v:c:s:r:dh" opt; do
+while getopts "v:c:s:r:adh" opt; do
   case $opt in
     v) VEHICLE=$OPTARG ;;
     c) CHARGERS=$OPTARG ;;
     r) RES=$OPTARG ;;
+    a) AUTOMATED_DEMO=true ;;
     d) DEBUG=true ;;
     h) show_help; exit 0 ;;
     *) echo "Invalid option"; exit 1 ;;
@@ -43,13 +45,18 @@ else
   pip install carla==0.9.14
 fi
 
-if $DEBUG; then
-  python3 manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/$CHARGERS.csv --res $RES;
+if $AUTOMATED_DEMO; then
+  python input/creation/charger_options.py 2 1 -i 0 --power 100_000 --efficiency 0.95 --seed 0 -m Town10HD > input/examples/automation-demo-chargers.csv &&
+  python multitracking.py input/examples/tracked_agents.csv output/Town10_lap -w input/examples/automation-demo-chargers.csv --seed 0 -t 230 -d 0.05
 else
-  python3 manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/$CHARGERS.csv --res $RES > /dev/null 2>&1 &
+  if $DEBUG; then
+    python manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/$CHARGERS.csv --res $RES;
+  else
+    python manual_control_steeringwheel.py ./input/examples/$VEHICLE.csv -w ./input/examples/$CHARGERS.csv --res $RES > /dev/null 2>&1 &
+  fi
 fi
 
-python3 navigation/draw_chargers.py ./input/examples/$CHARGERS.csv
+python navigation/draw_chargers.py ./input/examples/$CHARGERS.csv
 
 # Wi-Fi IP Address: 144.39.45.43
 # Ethernet IP Address: 129.123.153.74 
